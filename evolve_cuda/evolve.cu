2c204fea99314b503379b1a814bacef8f9a8c43e
Mohammed E. Amer <mogammed.e.amer@gmail.com>
2021-09-04 13:31:07 +0200

    - Add comments

--- evolve_cuda/evolve.cu
+++ evolve_cuda/evolve.cu
@@ -55,7 +55,10 @@ void evolve_kernel(int *cell_arr, int *out_arr, int n)
 {
   int num_elem = n*n*n;
 
+  // cell index
   int current_idx = blockIdx.x*blockDim.x+threadIdx.x;
+
+  // grid-stride loop
 
   for (int idx=current_idx;
     idx<num_elem; 
@@ -65,11 +68,15 @@ void evolve_kernel(int *cell_arr, int *out_arr, int n)
 
       int alive_count = 0;
 
+      // collect adjacent
+
       int adj_x[] = {idx_3d.x-1, idx_3d.x, idx_3d.x+1};
 
       int adj_y[] = {idx_3d.y-1, idx_3d.y, idx_3d.y+1};
 
       int adj_z[] = {idx_3d.z-1, idx_3d.z, idx_3d.z+1};
+
+      // count live
 
       for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
@@ -98,6 +105,8 @@ void evolve_kernel(int *cell_arr, int *out_arr, int n)
 
       int is_alive = cell_arr[current_idx];
 
+      // apply local rules
+
       out_arr[current_idx] = should_live(is_alive, alive_count);
 
     }
@@ -112,21 +121,31 @@ void evolve(int *cell_arr, int *out_arr, int n)
 
   num_elem = n*n*n;
 
+  // allocate unified mem
+
   cudaMallocManaged(&_in, num_elem*sizeof(int));
   cudaMallocManaged(&_out, num_elem*sizeof(int));
+
+  // copy input
 
   for (int i = 0; i < num_elem; i++) {
     _in[i] = cell_arr[i];
   }
+
+  // call kernel
 
   int threadsPerBlock = 256;
   int blocks = (num_elem+threadsPerBlock)/threadsPerBlock;
 
   evolve_kernel<<<blocks,threadsPerBlock>>>(_in, _out, n);
 
+  // sync with GPU
+
   cudaDeviceSynchronize();
 
   cudaFree(_in);
+
+  // copy output
 
   for (int i = 0; i < num_elem; i++) {
     out_arr[i] = _out[i];
